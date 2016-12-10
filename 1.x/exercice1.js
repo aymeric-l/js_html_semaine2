e934ebbba88ec96c7e7e007355dc9776a4ff230a
aymeric-l <aymeric-lacoste@hotmail.fr>
2016-12-11 00:09:30 +0100

    exercice 1.5 fini

--- 1.x/exercice1.js
+++ 1.x/exercice1.js
@@ -1,7 +1,6 @@
-var premier = parseInt(prompt("Tappez un premier nombre à virgule"));
-var deuxieme = prompt("Tappez un deuxieme nombre à virgule");
-var resultat = premier * deuxieme;
-alert(resultat);
-
-
-
+var age = parseInt(prompt("Indiquez votre âge"));
+	if (age < 18){
+		alert("Vous êtes mineur");
+	}else{
+		alert("Vous êtes majeur");
+}
