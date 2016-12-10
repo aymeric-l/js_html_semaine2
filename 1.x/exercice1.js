62dae2085fd96a99328984dd1b74824c7dce410d
aymeric-l <aymeric-lacoste@hotmail.fr>
2016-12-11 00:06:00 +0100

    exercice 1.4 fini

--- 1.x/exercice1.js
+++ 1.x/exercice1.js
@@ -1,4 +1,7 @@
-var prenom;
+var premier = parseInt(prompt("Tappez un premier nombre à virgule"));
+var deuxieme = prompt("Tappez un deuxieme nombre à virgule");
+var resultat = premier * deuxieme;
+alert(resultat);
 
-	prenom = prompt("Indiquez votre prénom");
-alert("Bonjour, "+prenom);
+
+
