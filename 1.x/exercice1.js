5ce35e140ca630e421ac78b16880da10ddaa6020
aymeric-l <aymeric-lacoste@hotmail.fr>
2016-12-10 23:11:59 +0100

    exercice 1.2 fini

--- 1.x/exercice1.js
+++ 1.x/exercice1.js
@@ -1,2 +1,5 @@
-var jairiencomprisauxinstructionsjulien = "Aidez moiiiii";
-	alert(jairiencomprisauxinstructionsjulien);
+var nom = "Doe";
+	var prenom = "John";
+	var ville = "New-York";
+
+alert("Nom : "+nom+"\nPrenom : "+prenom+"\nVille : "+ville);
