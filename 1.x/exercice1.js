ffb86a9b4c14bce6475d137826351db00825ebbc
aymeric-l <aymeric-lacoste@hotmail.fr>
2016-12-11 01:12:13 +0100

    exercice 1.7, (creation de satan) fini

--- 1.x/exercice1.js
+++ 1.x/exercice1.js
@@ -1,5 +1,22 @@
-	var nom = prompt("Indiquez votre nom");
-	var prenom = prompt("Indiquez votre prenom");
-	var ville = prompt("Indiquez votre ville");
+	var age = prompt("Indiquez votre age");
+	var sexe = prompt("Indiquez votre sexe (homme ou femme)");
+	/*if(sexe < 21cm){
+	return false;
+	}*/
+	var natio = prompt("Indiquez votre nationnalité(française, tibétaine...)");
 
-alert("Nom : "+nom+"\nPrenom : "+prenom+"\nVille : "+ville);
+if(age < 18){
+	age = "mineure";
+}else if (age >= 18){
+	age = "majeure";
+}
+
+if(sexe == "homme"){
+	natio = natio.substring(0,natio.length-1); //Retirer le dernier charactere de la variable
+	age = age.substring(0,age.length-1);
+	alert("Vous êtes un homme "+age+" "+natio);
+}else if(sexe == "femme"){
+	alert("Vous êtes une femme "+age+" "+natio);
+}else{
+	alert("Ni homme ni femme ?\n Désolé je ne parle pas aux "+sexe);
+}
